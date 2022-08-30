b916214cf670df2ed90d75cfa86cd7a0514f0f4e
WillyLiu <willywcliu@kknow.com>
2022-08-30 23:13:55 +0800

    modify test case for next scenario
    加上一個新的行為:
    假如 LogError 發生錯誤
    要拋出一個 error, 然後寄信給處理人員
    
    注意25行: 又一次高超的注入
    書中有提到如果注入的東西越來越多
    會建議包成一個類別
    
    注意28行: 一個高超的屬性注入
    這個屬性當然可以寫死在 FakeWebService 裡
    但把它變成 public 屬性就多了彈性

--- logAnalyzer_test.go
+++ logAnalyzer_test.go
@@ -6,15 +6,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_TooShortFileNameShouldLogError(t *testing.T) {
+// func Test_TooShortFileNameShouldLogError(t *testing.T) {
+// 	fw := &FakeWebService{}
+// 	la := NewLogAnalyzer(fw)
+
+// 	tooShortFileName := "abc.txt"
+// 	la.Analyze(tooShortFileName)
+
+// 	expected := "File name too short: " + "abc.txt"
+// 	actual := fw.lastError
+
+// 	assert.Equal(t, expected, actual, "they should be equal")
+// }
+
+func Test_TooShortFileNameShouldSendEmail(t *testing.T) {
 	fw := &FakeWebService{}
-	la := NewLogAnalyzer(fw)
+	fe := &FakeEmailService{}
+	la := NewLogAnalyzer(fw, fe)
 
 	tooShortFileName := "abc.txt"
+	fw.ShouldThrowError = true;
 	la.Analyze(tooShortFileName)
 	
-	expected := "File name too short: " + "abc.txt"
-	actual := fw.lastError
+	expected_to := "support@going.cloud"
+	expected_body := "Fake exception!"
+	expected_subject := "Can't Log"
+	
+	actual_to := fe.To
+	actual_body := fe.Body
+	actual_subject := fe.Subject
 
-	assert.Equal(t, expected, actual, "they should be equal")
+	assert.Equal(t, expected_to, actual_to, "they should be equal")
+	assert.Equal(t, expected_body, actual_body, "they should be equal")
+	assert.Equal(t, expected_subject, actual_subject, "they should be equal")
 }
