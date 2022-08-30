f3c85abe5c23e154caf461dc57eb0a612489dc50
WillyLiu <willywcliu@kknow.com>
2022-08-30 22:40:13 +0800

    implement fake web service class
    實作 IWebService 介面, 建立一個 FakeWebService 來模擬真實的 Web 服務

--- webService.go
+++ webService.go
@@ -3,3 +3,11 @@ package chap4
 type IWebService interface {
 	LogError(message string)
 }
+
+type FakeWebService struct {
+	lastError string
+}
+
+func (fw *FakeWebService) LogError(message string){
+	fw.lastError = message;
+}
